f918993d9d1267fa30f140e49c60ab56782209b1
tdev.js <tdev.js@gmail.com>
2020-09-25 20:09:38 +0530

    monsters roledex with deploy

---
+++ src/components/card/card.jsx
@@ -0,0 +1,14 @@
+import './card.css';
+
+import React from 'react';
+
+export const Card = props => (
+  <div className='card-container'>
+    <img
+      alt='monster'
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+    />
+    <h2 key={props.monster.id}>{props.monster.name}</h2>
+    <p>{props.monster.email}</p>
+  </div>
+);
---
+++ src/components/search-box/search-box.jsx
@@ -0,0 +1,12 @@
+import './search-box.css';
+
+import React from 'react';
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+  <input
+    className='search'
+    type='search'
+    placeholder={placeholder}
+    onChange={handleChange}
+  />
+);
